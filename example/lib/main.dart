9050b856bb9e830ee4a2e273d32c6959d3fa339b
rhylvin2022 <rhylvin.tinador@hitachi-dps.com>
2025-02-27 14:17:51 +0800

    ready

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _deviceStatus = 'Checking...';
-  final _emulatorCheckerPlugin = EmulatorChecker();
 
   @override
   void initState() {
